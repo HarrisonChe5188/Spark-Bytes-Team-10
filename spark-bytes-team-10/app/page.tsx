30f7432378ce0d0429db7f3b0cb70ca83c40b208
Justin Wang <justin1@bu.edu>
2025-11-04 20:59:39 -0500

    remove unused auth pages

--- spark-bytes-team-10/app/page.tsx
+++ spark-bytes-team-10/app/page.tsx
@@ -1,10 +1,13 @@
 import { AuthButton } from "@/components/auth-button";
+import { ThemeSwitcher } from "@/components/theme-switcher";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
       <AuthButton />
+      this is a built in component we can use
+      <ThemeSwitcher />
     </div>
   );
 }
