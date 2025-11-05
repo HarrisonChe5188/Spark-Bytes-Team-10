d18b3077299bb9767ab55d49a282b4fea0555b7d
Justin Wang <justin1@bu.edu>
2025-11-04 19:26:42 -0500

    initialize Next.js Supabase starter

---
+++ spark-bytes-team-10/app/page.tsx
@@ -0,0 +1,10 @@
+import { AuthButton } from "@/components/auth-button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <AuthButton />
+    </div>
+  );
+}
