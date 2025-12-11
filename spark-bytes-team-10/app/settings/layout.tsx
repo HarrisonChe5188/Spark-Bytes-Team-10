2d04c2949e7dff2fbe8aafa0df4c6791b2f44094
Justin Wang <justin1@bu.edu>
2025-12-11 13:32:44 -0500

    feat: add delete account feature and refactor profile to settings

--- spark-bytes-team-10/app/settings/layout.tsx
+++ spark-bytes-team-10/app/settings/layout.tsx
@@ -1,7 +1,7 @@
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 
-export default function ProfileLayout({
+export default function SettingsLayout({
   children,
 }: {
   children: React.ReactNode;
